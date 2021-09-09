e75d8dcbe004112ee1b4e3d80e99f5baff3edbdd
KBANKNET\pitchakorn.jo <pitchakorn.jo@kbtg.tech>
2021-09-09 19:28:01 +0700

    init

---
+++ src/main/java/com/example/exam/harrypotter/HarrypotterApplication.java
@@ -0,0 +1,13 @@
+package com.example.exam.harrypotter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarrypotterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarrypotterApplication.class, args);
+	}
+
+}
